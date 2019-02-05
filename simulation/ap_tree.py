9de84e834945eec11f092efa7393d91e4c6ae287
lif46 <lidn0123@gmail.com>
2019-02-05 00:34:30 -0500

    variabletree

---
+++ simulation/ap_tree.py
@@ -0,0 +1,24 @@
+import os
+import win32com.client as win32
+
+
+aspen = win32.Dispatch('Apwn.Document')
+aspen.InitFromArchive2(os.path.join(os.path.dirname(__file__), 'hbcdumodel/CDU-basic.bkp'))
+
+f=open('tree.txt','x')
+def print_elements(obj, level=0):
+    
+    if hasattr(obj, 'Elements'):
+        print(' '*level + obj.Name)
+        f.write(' '*level + obj.Name)
+        for o in obj.Elements:
+            print_elements(o, level + 1)
+    else:
+        print(' '*level, obj.Name, ' = ', obj.Value)
+        f.write(' '*level, obj.Name, ' = ', obj.Value)
+
+
+print_elements(aspen.Tree) 
+f.close()
+
+aspen.Close
